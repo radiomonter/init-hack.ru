a72e750f0b6bd0227364e5440e1c98c5fa5698a7
Maksim Terehov <radiomonter@yandex.ru>
2020-02-08 21:19:29 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,235 @@
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>init-hack.tanks</title>
+	<link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<script type="text/javascript" src="js/jquery.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="http://cdn.jsdelivr.net/jquery.slick/1.6.0/slick.css"/>
+    <link rel="stylesheet" type="text/css" href="http://cdn.jsdelivr.net/jquery.slick/1.6.0/slick-theme.css"/>
+</head>
+<body>
+	<div class="header">
+		<!--div class="header-container">
+			<div class="logo"><a href="#"><img src="images/logo.png"></a></div>
+			<div class="menu">
+				<div class="menu-item active">Главная</div>
+				<div class="menu-item">Новости</div>
+				<div class="menu-item">Как начать?</div>
+				<div class="menu-item">Форум</div>
+				<div class="menu-item">Донат</div>
+				<div class="menu-item">Рейтинги</div>
+			</div>
+			<!--div class="language">
+				<div class="button_lng">
+					<div class="button_lng_flag"></div>
+					<div class="button_lng_text">Русский</div>
+					<div class="button_lng_arrow"></div>
+				</div>
+			</div-->
+		</div-->
+		<div class="header-container-content">
+			<div class="hyp">
+				<div class="slider-container">
+					<div class="item-slider">
+						<div class="header-slider">Можно играть через Standalone Flash Player</div>
+						<div class="text-slider">Ссылка на игру http://188.127.237.44/battle/Preloader7.swf?locale=ru&rand= </div>
+					</div>
+					
+					
+				</div>
+				<a href="battle/"><div class="btn-slider"></div></a>
+
+			</div>
+		</div>
+	</div>
+	<div class="advantages">
+		<div class="advantages-container">
+			<div class="header-advantages">Играй с нами</div>
+			<div class="subheader-advantages">У нас выгоднее, лучше и интереснее</div>
+			<div class="cards-container-advantages">
+				<div class="card-advantage">
+					<div class="advantage-light"></div>
+					<div class="card-advantage-img"><img src="images/advantage-1.png"></div>
+					<div class="card-advantage-header">Ничего не теряешь</div>
+					<div class="card-advantage-text">Покупай боевой пропуск в любой момент! Получишь все пропущенные призы.</div>
+				</div>
+				<div class="card-advantage">
+					<div class="advantage-light"></div>
+					<div class="card-advantage-img"><img src="images/advantage-2.png"></div>
+					<div class="card-advantage-header">Ничего не теряешь</div>
+					<div class="card-advantage-text">Покупай боевой пропуск в любой момент! Получишь все пропущенные призы.</div>
+				</div>
+				<div class="card-advantage">
+					<div class="advantage-light"></div>
+					<div class="card-advantage-img"><img src="images/advantage-3.png"></div>
+					<div class="card-advantage-header">Ничего не теряешь</div>
+					<div class="card-advantage-text">Покупай боевой пропуск в любой момент! Получишь все пропущенные призы.</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!--div class="hyp"></div-->
+	<!--div class="promo-video">
+		<div class="promo-video-container">
+			<div class="promo-video-header">Промо ролик</div>
+			<div class="promo-video-subheader">Познакомьтесь с нашим проектом за 5 минут</div>
+			<div class="promo-video-play">
+				<iframe width="728" height="409" src="https://www.youtube.com/embed/YKdJzhr6yT4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+			</div>
+			<div class="promo-video-footer">
+				<div class="promo-footer-or">ИЛИ</div>
+				<div class="promo-footer-how">узнайте <br>как начать играть</div>
+				<!--<div class="promo-footer-youtube"></div>-->
+			<!--/div>
+		</div-->
+	<!--/div-->
+	<!--div class="poll">
+		<div class="poll-header">Как вы относитесь к последнему обновлению?</div>
+		<div class="poll-container">
+			<div class="poll-element">
+				<div class="poll-percent">82%</div>
+				<div class="poll-desc">Отлично, мне понравилось</div>
+				<div class="poll-line">
+					<div class="poll-inline" style="width:82%"></div>
+				</div>
+				<div class="poll-btn">голосовать</div>
+			</div>
+			<div class="poll-element">
+				<div class="poll-percent">12%</div>
+				<div class="poll-desc">Не плохо, но есть баги</div>
+				<div class="poll-line">
+					<div class="poll-inline" style="width:12%"></div>
+				</div>
+				<div class="poll-btn">голосовать</div>
+			</div>
+			<div class="poll-element">
+				<div class="poll-percent">6%</div>
+				<div class="poll-desc">Плохое обновление</div>
+				<div class="poll-line">
+					<div class="poll-inline" style="width:6%"></div>
+				</div>
+				<div class="poll-btn">голосовать</div>
+			</div>
+		</div>
+	</div-->
+	<!--div class="hyp"></div-->
+	<!--div class="news">
+		<div class="news-header">Новости</div>
+		<div class="news-subheader">Оставайтесь в курсе событий проекта</div>
+		<div class="news-container">
+			<div class="news-element">
+				<div class="news-element-img">
+					<img src="images/new1.png">
+				</div>
+				<div class="news-element-desc">
+					<div class="news-element-text">Новогоднее обновление</div>
+					<div class="news-element-date">
+						<div class="calendar"></div>
+						<div class="news-element-date-text">02.01.2019</div>
+					</div>
+				</div>
+			</div>
+			<div class="news-element">
+				<div class="news-element-img">
+					<img src="images/new1.png">
+				</div>
+				<div class="news-element-desc">
+					<div class="news-element-text">Главнй приз</div>
+					<div class="news-element-date">
+						<div class="calendar"></div>
+						<div class="news-element-date-text">02.01.2019</div>
+					</div>
+				</div>
+			</div>
+			<div class="news-element">
+				<div class="news-element-img">
+					<img src="images/new1.png">
+				</div>
+				<div class="news-element-desc">
+					<div class="news-element-text">Скины, пушки, корпуса</div>
+					<div class="news-element-date">
+						<div class="calendar"></div>
+						<div class="news-element-date-text">02.01.2019</div>
+					</div>
+				</div>
+			</div>
+			<div class="news-element">
+				<div class="news-element-img">
+					<img src="images/new1.png">
+				</div>
+				<div class="news-element-desc">
+					<div class="news-element-text">Супер акция лабиринт</div>
+					<div class="news-element-date">
+						<div class="calendar"></div>
+						<div class="news-element-date-text">02.01.2019</div>
+					</div>
+				</div>
+			</div>
+			<div class="news-element">
+				<div class="news-element-img">
+					<img src="images/new1.png">
+				</div>
+				<div class="news-element-desc">
+					<div class="news-element-text">Перенос аккаунтов</div>
+					<div class="news-element-date">
+						<div class="calendar"></div>
+						<div class="news-element-date-text">02.01.2019</div>
+					</div>
+				</div>
+			</div>
+			<div class="news-element">
+				<div class="news-element-img">
+					<img src="images/new1.png">
+				</div>
+				<div class="news-element-desc">
+					<div class="news-element-text">Нагиб на викогроме</div>
+					<div class="news-element-date">
+						<div class="calendar"></div>
+						<div class="news-element-date-text">02.01.2019</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="more-news"><a>Еще больше новостей</a></div>
+	</div-->
+	<!--div class="footer">
+		<div class="footer-container">
+			<div class="footer-left">
+				<!--div class="logo-footer"></div-->
+				<!--div class="logo-footer-desc">
+					<div class="logo-footer-head"></div>
+					<div class="logo-footer-text">Все права защищены.<br>Любое копирование материала запрещено</div>
+				</div-->
+			</div>
+			<!--div class="footer-right">
+				<script type="text/javascript" src="https://vk.com/js/api/openapi.js?160"></script>
+				<!-- VK Widget -->
+				<!--div id="vk_groups"></div>
+				<script type="text/javascript">
+				VK.Widgets.Group("vk_groups", {mode: 1}, 97776287);
+				</script>
+			</div-->
+		</div>
+	</div-->
+	<script type="text/javascript" src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+	<script type="text/javascript" src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+	<script type="text/javascript" src="http://cdn.jsdelivr.net/jquery.slick/1.6.0/slick.min.js"></script>  
+	<script type="text/javascript">
+		$('.slider-container').slick({
+			dots: true,
+			infinite: true,
+			arrows: false,
+			speed: 500,
+			autoplay:true,
+			autoplaySpeed:4000
+		});
+	</script>
+</body>
+</html>
+<?}?>
